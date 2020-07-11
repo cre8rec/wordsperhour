34bec606b039b609d18a8f61699763e30a1c31b4
cre8rec <juliantmayes@gmail.com>
2020-07-11 09:57:26 -0700

    Updates

---
+++ precache-manifest.3b9a6aba1eed357bbda7ca9fed39b87b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f423ba3d6594e481815f9ca37a2eceb",
+    "url": "/wordsperhour/index.html"
+  },
+  {
+    "revision": "eda0d4a0df30ed93e2ac",
+    "url": "/wordsperhour/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "ce9b1a1bfddc7a138359",
+    "url": "/wordsperhour/static/css/main.1db3e3ba.chunk.css"
+  },
+  {
+    "revision": "eda0d4a0df30ed93e2ac",
+    "url": "/wordsperhour/static/js/2.cbaaa291.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/wordsperhour/static/js/2.cbaaa291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9b1a1bfddc7a138359",
+    "url": "/wordsperhour/static/js/main.168790da.chunk.js"
+  },
+  {
+    "revision": "cb7e5cf6b99215f2462d",
+    "url": "/wordsperhour/static/js/runtime-main.260054d6.js"
+  }
+]);
